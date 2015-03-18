02bc3bea01720564059ec37f038a950f4c3d7fd8
Itoh, Masaru <masaru@borderless-japan.com>
2015-03-18 14:08:02 +0900

    update for Meteor 1.0.4

--- basic-auth.js
+++ basic-auth.js
@@ -7,20 +7,22 @@ HttpBasicAuth = (function() {
   HttpBasicAuth.prototype = {
     constructor: HttpBasicAuth,
     protect: function(routes) {
+      var basicAuth = WebAppInternals.NpmModules.connect.module.basicAuth;
+
       routes = routes || [''];
       if (!_.isArray(routes))
         throw new Error("'routes' must be an array of route Strings");
-      
+
       // Splice the middleware stack and slip in the auth function
       // so it is called first at desired routes (in specified order)
       for(var i=0; i<routes.length; i++) {
         WebApp.connectHandlers.stack.splice(i, 0, {
           route: routes[i],
-          handle: WebApp.__basicAuth__(this.callback, this.realm)
+          handle: basicAuth(this.callback, this.realm)
         });
       }
     }
   };
-  
+
   return HttpBasicAuth;
 })();
